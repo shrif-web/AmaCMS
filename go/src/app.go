317e5a884da5e9a910b2326f5ed059c1a67a3501
ahmadsalimi <ahsa9978@gmail.com>
2021-02-08 15:44:29 +0330

    config go server

--- go/src/app.go
+++ go/src/app.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		log.Print("Running Ping")
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	
+
+	log.Printf("Server is starting...")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
