bdeb0e23b0af1ec6ec9795d22cb6fd2c06eff752
arman <armanzarei1378@gmail.com>
2021-02-10 17:31:37 +0330

    move folder views to src

--- app/src/app.js
+++ app/src/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.set('view engine', 'ejs');
+app.set('views', 'src/views')
 app.disable('view cache'); // for development
 
 app.use('/api', userRouter)
