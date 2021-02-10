c36a6cd546a69f8ff82b9b34b1b4a817f500cc05
ahmadsalimi <ahsa9978@gmail.com>
2021-02-10 17:58:13 +0330

    auto create posts index

--- app/src/services/elastic.js
+++ app/src/services/elastic.js
@@ -1,9 +1,36 @@
 import { Client } from '@elastic/elasticsearch';
 
-export default new Client({
+const client = new Client({
     node: process.env.ELASTIC_HOST,
     auth: {
         username: 'elastic',
         password: process.env.ELASTIC_PASSWORD
     }
 })
+
+const res = await client.indices.exists({ index: 'posts' })
+
+if (!res.body) {
+    await client.indices.create({
+        index: 'posts'
+    })
+
+    await client.indices.putSettings({
+        index: 'posts',
+        body: {
+            settings: {
+                index: {
+                    blocks: {
+                        read_only_allow_delete: null
+                    }
+                }
+            }
+        }
+    })
+
+    console.log('index posts created in elasticsearch')
+} else {
+    console.log('index posts already exists in elasticsearch')
+}
+
+export default client
