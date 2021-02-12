8c2470f6f40c11cba0ad2c956ca4ec975695edd0
ahmadsalimi <ahsa9978@gmail.com>
2021-02-12 21:54:48 +0330

    package admin finished

--- app/src/routers/admin/admin.router.js
+++ app/src/routers/admin/admin.router.js
@@ -3,6 +3,7 @@ import { index } from "../../controllers/admin/admin.controller.js"
 import categoryRouter from "./category.router.js"
 import tagRouter from "./tag.router.js"
 import postRouter from "./post.router.js"
+import packageRouter from "./package.router.js"
 import userRouter from "./user.router.js"
 
 const router = express.Router()
@@ -11,6 +12,7 @@ router.route('/').get(index)
 router.use('/tag', tagRouter)
 router.use('/category', categoryRouter)
 router.use('/post', postRouter)
+router.use('/package', packageRouter)
 router.use('/user', userRouter)
 
 
