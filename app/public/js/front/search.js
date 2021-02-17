04809cc705ba38796319ba8189b0b9c999fc16ce
ahmadsalimi <ahsa9978@gmail.com>
2021-02-17 16:14:54 +0330

    create search script

---
+++ app/public/js/front/search.js
@@ -0,0 +1,18 @@
+const showSearchResults = results => {
+    /*  results: array of {
+            title,
+            imageUrl, 
+            content,
+            views,
+            createdAt,
+            likes,
+            user: {
+                firstName,
+                lastName,
+            },
+        } */
+}
+
+const search = () => {
+    
+}
