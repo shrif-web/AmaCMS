60f56940999c67aaabbaa07868d8017cfb7baf1e
arman <armanzarei1378@gmail.com>
2021-02-17 14:39:20 +0330

    add priceFormat to Number prototype

--- app/src/startup.js
+++ app/src/startup.js
@@ -26,6 +26,10 @@ const createSuperUser = async () => {
     }
 }
 
+Number.prototype.priceFormat = function() {
+    return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
 export default async () => {
     await sequelize.sync()
     await createSuperUser()
