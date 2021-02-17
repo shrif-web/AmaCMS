b986f4998893d21a468499caf756358201ea786b
ahmadsalimi <ahsa9978@gmail.com>
2021-02-17 16:40:07 +0330

    finish search api call

--- app/public/js/front/search.js
+++ app/public/js/front/search.js
@@ -8,7 +8,7 @@ const showSearchResults = results => {
             id,
             title,
             imageUrl, 
-            content,
+            content : array of text,
             views,
             createdAt,
             likes,
@@ -27,28 +27,43 @@ const sleep = time => {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+const getContent = result => {
+    if (result.highlight) {
+        return result.highlight.content.map(highlight => `...${highlight}...`)
+    }
+    return [`${result._source.content.slice(0, 100)}...`]
+}
+
 const prepareResults = response => {
     const results = []
     for (const result of response.hits.hits) {
 
         results.push({
-
+            id: result._id,
+            title: result._source.title,
+            imageUrl: result._source.imageUrl,
+            content: getContent(result),
+            views: result._source.views,
+            createdAt: result._source.createdAt,
+            likes: result._source.likes,
+            user: result._source.user,
         })
     }
+    return results
 }
 
 const search = () => {
     const query = $('#search_input').val()
     console.log(`searcing ${query}`);
 
-    
+    loading()
 
     $.get(`/api/search?q=${query}`)
         .done(response => {
             sleep(1000) // just for dev
                 .then(() => {
-
+                    const results = prepareResults(response)
+                    showSearchResults(results)
                 })
-            window.location = '/admin/post/'
-        }).fail(response => alert(response.responseJSON.message))
+        }).fail(response => error(response))
 }
