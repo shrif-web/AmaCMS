b3f30dfb6906ad99217e475c21a26ea05a708757
arman <armanzarei1378@gmail.com>
2021-02-17 15:03:42 +0330

    Most favorite posts in admin dashboard

--- app/src/controllers/admin/admin.controller.js
+++ app/src/controllers/admin/admin.controller.js
@@ -1,9 +1,14 @@
 import SocialMedia from "../../models/socialMedia.model.js"
+import Post from "../../models/post.model.js"
 
 export const index = async (req, res) => {
     const socialMedias = await SocialMedia.findAll();
+    const favoritePosts = await Post.findAll({
+        limit: 4
+    })
 
     res.render('admin/home/index', {
-        socialMedias: socialMedias,
+        socialMedias,
+        favoritePosts
     });
 }; 
