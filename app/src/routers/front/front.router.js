b1b0a45811eebdf4c0923848e7c8b51fd91d6c20
arman <armanzarei1378@gmail.com>
2021-02-18 01:21:05 +0330

    Package page added

--- app/src/routers/front/front.router.js
+++ app/src/routers/front/front.router.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { getPost } from "../../controllers/front/post.controller.js"
+import { getPackage } from "../../controllers/front/package.controller.js"
 import { index } from "../../controllers/front/home.controller.js"
 
 const router = express.Router()
 
 router.route('/post/:id').get(getPost)
+router.route('/package/:id').get(getPackage)
 router.route('/').get(index)
 
 export default router
