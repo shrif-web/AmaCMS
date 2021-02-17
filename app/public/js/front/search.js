e141e9ab7b1708c46b59646a9bc20d0f47bf8f2a
ahmadsalimi <ahsa9978@gmail.com>
2021-02-17 16:21:27 +0330

    update search.js

--- app/public/js/front/search.js
+++ app/public/js/front/search.js
@@ -1,3 +1,7 @@
+const loading = () => {
+    // TODO: Arman
+}
+
 const showSearchResults = results => {
     /*  results: array of {
             title,
@@ -13,6 +17,27 @@ const showSearchResults = results => {
         } */
 }
 
+const sleep = time => {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+const prepareResults = response => {
+
+}
+
+
 const search = () => {
-    
+    const query = $('#search_input').val()
+    console.log(`searcing ${query}`);
+
+    // TODO: switch to loading mode
+
+    $.get(`/api/search?q=${query}`)
+        .done(response => {
+            sleep(1000) // just for dev
+                .then(() => {
+
+                })
+            window.location = '/admin/post/'
+        }).fail(response => alert(response.responseJSON.message))
 }
